9cce6daf7f6ffb1d4efbcc8ce744973330234ca5
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-18 14:10:10 +0530

    Update newton.py

--- newton.py
+++ newton.py
@@ -13,6 +13,7 @@ Created on Mon Dec  9 12:33:09 2019
 
 @author: chintan
 """
+import numpy as np
 from matplotlib import pyplot as plt
 from numpy import sin,cos,exp
 from scipy.optimize import newton
@@ -20,8 +21,11 @@ def f(x):
     return(np.sin(np.cos(np.exp(x))))
 def fp(x):                #defining the derivative
     return(-exp(x)*cos(cos(exp(x)))*sin(exp(x)))    
-root=newton(f,-.1,fp)       
+root=newton(f,-.1,fp)        
 x=np.arange(-1,5,.01)
 y=f(x)
 plt.plot(x,y)
-print(root,f(root))   
+print('guess value:-0.1',root,f(root))
+root=newton(f,-1,fp)
+print('guess value:-1',root,f(root))
+   
